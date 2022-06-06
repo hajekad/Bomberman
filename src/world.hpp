02fc4270f7acb88f055d14be100bfb1f3242e94c
Adam Hájek <hajekad3@fit.cvut.cz>
2022-06-06 21:53:32 +0200

    documentation done

--- src/world.hpp
+++ src/world.hpp
@@ -20,23 +20,39 @@ class CWorld
 {
 private:
     int playerCnt; /** *@brief count of players on the current map */
-    bool hasPlayer;
-    bool hasEnemy;
-    int scorePO;
-    int scorePT;
-    std::shared_ptr<CCharacter> _p1;
-    std::shared_ptr<CCharacter> _p2;
+    
+    bool hasPlayer; /** *@brief true if a player is present on this map */
+    
+    bool hasEnemy; /** *@brief true if a enemy is on this map */
+    
+    int scorePO; /** *@brief score of player one */
+    
+    int scorePT; /** *@brief score of player two */
+    
+    std::shared_ptr<CCharacter> _p1; /** *@brief player 1 */
+    
+    std::shared_ptr<CCharacter> _p2; /** *@brief player 2 */
 
 public:
-    std::vector<std::vector<CCell>> worldMap;
+    std::vector<std::vector<CCell>> worldMap; /** *@brief map built from cells */
     
-    std::vector<std::shared_ptr<CCharacter>> characters;
+    std::vector<std::shared_ptr<CCharacter>> characters; /** *@brief all characters that are still alive */
     
     CWorld(std::string fileName, WINDOW * _w);
 
+    /**
+     * @brief influences state of cells with coordinates from _d
+     * 
+     * @param _d vector of all influenced cells
+     * @param _p owner of the exploding bomb
+     */
     void destroy(std::vector<std::pair<int, int>> & _d, CPlayer * _p);
 
+    /**
+     * @brief updates the state of map in time, detonates bomb and decides the next move of the game
+     * 
+     * @param _i input from player
+     * @return int final high score or 1
+     */
     int update(char _i);
-    
-    //~CWorld();
 };
